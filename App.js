61d952393154cd1c23091a5de61e1e47ea1a24ce
zanonijordan <zanoni.jordan07@gmail.com>
2025-05-05 19:13:20 +0100

    creat project-test

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Input } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Input placeholder="Enter your name" />
       <StatusBar style="auto" />
     </View>
   );
